48619f7c58ba4556a039551b1fbe0c0ff4713e64
Gabriel Torres <al2012.163.torres@gmail.com>
2021-12-02 23:32:53 -0300

    feat: use joi to validate amount of top-song

--- src/controllers/recommendationsController.js
+++ src/controllers/recommendationsController.js
@@ -1,11 +1,11 @@
 import SongNotFound from '../errors/SongNotFound.js';
-import validateRecommendation from '../validations/validations.js';
 import ValidationError from '../errors/ValidationError.js';
+import * as validations from '../validations/validations.js';
 import * as recommendationService from '../services/recommendationService.js';
 
 export async function createRecommendation(req, res, next) {
   try {
-    await validateRecommendation(req.body);
+    await validations.validateRecommendation(req.body);
     await recommendationService.createRecommendation(req.body);
 
     res.sendStatus(201);
@@ -18,10 +18,12 @@ export async function createRecommendation(req, res, next) {
 
 export async function getTopRecommendations(req, res, next) {
   try {
+    await validations.validateAmount(req.params);
     const recommendations = await recommendationService.getTopRecommendations(req.params.amount);
 
     res.send(recommendations);
   } catch (err) {
+    if (err instanceof ValidationError) return res.status(400).send(err.message);
     if (err instanceof SongNotFound) return res.status(404).send(err.message);
     next(err);
   }
