9d913535ff164f689d847ac7db56e6b810f76b7a
Gabriel Torres <al2012.163.torres@gmail.com>
2021-12-03 00:30:37 -0300

    refact: code consistency

--- src/services/votesService.js
+++ src/services/votesService.js
@@ -5,7 +5,7 @@ export async function vote(id, isUpvote) {
   const score = await votesRepository.getSongScore(id);
   if (score === undefined) throw new SongNotFound('This song does not exist');
 
-  const newScore = isUpvote ? (score + 1) : (score - 1);
+  const newScore = isUpvote ? score + 1 : score - 1;
 
   await votesRepository.vote(newScore, id);
   if (newScore < -5) await votesRepository.deleteSong(id);
