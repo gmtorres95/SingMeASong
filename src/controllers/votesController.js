08b4d7b228231b599f0dcbb94b42487a97ef9094
Gabriel Torres <al2012.163.torres@gmail.com>
2021-12-06 00:16:09 -0300

    chore: votesService.vote now returns message

--- src/controllers/votesController.js
+++ src/controllers/votesController.js
@@ -4,9 +4,9 @@ import * as votesService from '../services/votesService.js';
 export async function vote(req, res, next) {
   try {
     const isUpvote = req.url.split('/')[3] === 'upvote';
-    await votesService.vote(req.params.id, isUpvote);
+    const result = await votesService.vote(req.params.id, isUpvote);
 
-    res.sendStatus(200);
+    res.status(200).send(result.message);
   } catch (err) {
     if (err instanceof SongNotFound) return res.status(404).send(err.message);
     next(err);
