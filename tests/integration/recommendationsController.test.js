67f45e1b085f705492ba213533a91d70c1207be0
Gabriel Torres <al2012.163.torres@gmail.com>
2021-12-06 03:20:33 -0300

    test: integration - recommendationsController

--- tests/integration/recommendationsController.test.js
+++ tests/integration/recommendationsController.test.js
@@ -4,6 +4,17 @@ import app from '../../src/app.js';
 import connection from '../../src/database.js';
 import recommendationFactory from './factories/recommendationFactory.js';
 
+beforeEach(async () => {
+  const {
+    name,
+    youtubeLink,
+  } = recommendationFactory();
+  await connection.query(
+    'INSERT INTO songs (name, link) VALUES ($1, $2)',
+    [name, youtubeLink],
+  );
+});
+
 afterAll(async () => {
   await connection.query('DELETE FROM songs');
   connection.end();
@@ -43,5 +54,26 @@ describe('/recommendations', () => {
   it("Returns 400 for missing params", async () => {
     const result = await supertest(app).post('/recommendations').send();
     expect(result.status).toBe(400);
+  });
+});
+
+describe('/recommendations/top/:amount', () => {
+  it("Returns 200 for valid request", async () => {
+    const amount = 5;
+    const result = await supertest(app).get(`/recommendations/top/${amount}`);
+    expect(result.body.length).toBe(amount);
+  });
+
+  it("Returns 400 for invalid amount", async () => {
+    const amount = -1;
+    const result = await supertest(app).get(`/recommendations/top/${amount}`);
+    expect(result.status).toBe(400);
+  });
+
+  it("Returns 404 if there is no song to return", async () => {
+    await connection.query('DELETE FROM songs');
+    const amount = 1;
+    const result = await supertest(app).get(`/recommendations/top/${amount}`);
+    expect(result.status).toBe(404);
   });
 });
